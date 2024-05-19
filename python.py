9a0cd1c04ff0d7e6095ea0ddc1b6414a2ccb3389
VelardeSoft <u20211c221@upc.edu.pe>
2024-05-19 13:49:50 -0500

    1. Comentarios, Operadores aritmeticos.

---
+++ python.py
@@ -0,0 +1,39 @@
+# Variables en python
+
+cadena = 'nestor' #Los cadenas o string se puede representar con (") o (')
+valor = True
+num1 = 10
+num2 = 6.7
+suma = num1 + num2
+print (suma)
+print (type(suma))
+
+
+# Tipado dinamico
+v = 10
+print (v)
+v = 'Velarde'
+print (v)
+
+
+# comentario de multilinia
+'''
+    The course in python 
+    is very interesting
+'''
+
+
+# Operadores aritmeticos
+num1 = 10
+num2 = 3
+resultado = num1 // num2  # (//) son para entero
+print (resultado)
+residuo = num1 % num2  # (% modulo) retorna el residuo
+print (residuo)
+exponente = num1 ** num2  # (**) son para exponente
+print (exponente)
+
+
+# Ejercicio 01
+res = 3 ** 3 * (13/5-(2*4))
+print (res)
